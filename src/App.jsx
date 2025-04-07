5f96be248a0b0a732662cb5bd6db95783cd114e9
dongyeon <ldy1951123@gmail.com>
2025-04-08 02:45:52 +0900

    할일이 몇개인지 & 글 업로드 / 삭제 기능 구현 & map반복문 사용하여 리스트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,17 @@ import './App.css'
 
 function App() {
 
-  let [리스트제목, 리스트제목변경] = useState(['할일 1', '할일 2', '할일 3']);
-  let [메모, 메모변경] = useState(['메모 1', '메모 2', '메모 3']);
-  let [입력값, 입력값변경] = useState('');
+  let [제목, 제목변경] = useState(['할일 1', '할일 2']);
+  let [메모, 메모변경] = useState(['메모 1', '메모 2']);
+  let [제목입력값, 제목입력값변경] = useState('');
+  let [메모입력값, 메모입력값변경] = useState('');
 
   let addList = () => {
-    if (입력값.trim() !== "") {
-      리스트제목변경([...리스트제목, 입력값]);
-      입력값변경 ("");
+    if (제목입력값.trim() !== "" && 메모입력값.trim() !== "") {
+      제목변경([...제목, 제목입력값]);
+      메모변경([...메모, 메모입력값]);
+      제목입력값변경 ("");
+      메모입력값변경 ("");
     }
   }
 
@@ -20,9 +23,30 @@ function App() {
     <div className='App'>
       <div className='title-box'>
         <h1 className='title'>Todo List</h1>
-        <p className='title-content'>개의 할 일이 있습니다.</p>
+        <p className='title-content'>{제목.length}개의 할 일이 있습니다.</p>
       </div>
-      <div className='list'>
+      {
+        제목.map(function(a, i){
+          return (
+            <div className='list' key={i}>
+              <h3>{제목[i]}</h3>
+              <p>{메모[i]}</p>
+              <button className='delete-button'
+              onClick={() => {
+                let copy제목 = [...제목];
+                let copy메모 = [...메모];
+
+                copy제목.splice(i, 1);
+                copy메모.splice(i, 1);
+
+                제목변경(copy제목);
+                메모변경(copy메모);
+              }}>삭제</button>
+            </div>
+          )
+        })
+      }
+      {/* <div className='list'>
         <h3>{리스트제목[0]}</h3>
         <p>{메모[0]}</p>
         <button className='delete-button'>삭제</button>
@@ -36,11 +60,13 @@ function App() {
         <h3>{리스트제목[2]}</h3>
         <p>{메모[2]}</p>
         <button className='delete-button'>삭제</button>
-      </div>
+      </div> */}
       <div className='input-box'>
         <h3>해야할 일을 입력하세요.</h3>
-        <input type="text" className='todo-box' placeholder='todo List 작성'/>
-        <input type="text" className='todo-box2' placeholder='메모'/>
+        <input type="text" className='todo-box' placeholder='todo List 작성'
+        onChange = {(e) => {제목입력값변경(e.target.value); }} />
+        <input type="text" className='todo-box2' placeholder='메모' 
+        onChange = {(e) => {메모입력값변경(e.target.value); }}/>
         <button className='upload'
         onClick={addList}>업로드</button>
       </div>
